57abc1b7fc92167bd2773db3a0fd9e667784dc53
Brian Vega <BrianVega01@outlook.es>
2024-07-30 11:38:01 -0600

    51. Skipping JUnit test display Names

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,50 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.repositories.OwnerRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//@Disabled(value = "Dsiabled until we learn mocking")
+class OwnerSDJpaServiceTest {
+
+    private OwnerSDJpaService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new OwnerSDJpaService(null, null, null);
+    }
+
+    @Disabled(value = "Disabled until we learn mocking!")
+    @Test
+    void findByLastName() {
+        Owner foundOwner = service.findByLastName("Buck");
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
