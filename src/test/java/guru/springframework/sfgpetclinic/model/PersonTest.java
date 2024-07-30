9446a945f0c7dd9802890b10035a2fd68fd86f4b
Brian Vega <BrianVega01@outlook.es>
2024-07-30 17:44:55 -0600

    68. JUnit Test Dependency Injection

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,10 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,5 +34,10 @@ class PersonTest implements ModelTests {
     @DisplayName("My repeated test")
     void myRepeatedTest() {
         System.out.println("repeated test");
+    }
+
+    @RepeatedTest(value = 5)
+    void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + ": " + repetitionInfo.getCurrentRepetition());
     }
 }
