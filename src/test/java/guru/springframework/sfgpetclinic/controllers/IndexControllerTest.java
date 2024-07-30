60b0dece66cfcaad1f8b0dcaed0f418dd9db955c
Brian Vega <BrianVega01@outlook.es>
2024-07-30 10:51:30 -0600

    48. JUnit Assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexControllerTest {
+
+    IndexController indexController;
+
+    @BeforeEach
+    void setUp() {
+        indexController = new IndexController();
+    }
+
+    @Test
+    void index() {
+        assertEquals("index", indexController.index());
+        assertEquals("index", indexController.index(), "Wrong view Returned");
+        assertEquals("index", indexController.index(), "Another Expensive Message " +
+                "make me only if you have to");
+    }
+
+    @Test
+    void oupsHandler() {
+        assertTrue("notimplemented".equals(indexController.oupsHandler()), () -> "This is some expensive" +
+                " message to build" +
+                "for my test");
+    }
+}
