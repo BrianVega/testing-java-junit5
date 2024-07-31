869a3335423bcccae0af1da2b633f2b675ebc64e
Brian Vega <BrianVega01@outlook.es>
2024-07-31 12:06:27 -0600

    79. JUnit Extensions

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetTypeSDJpaServiceIT.java
@@ -1,11 +1,14 @@
 package guru.springframework.sfgpetclinic.services.springdatajpa;
 
+import guru.springframework.sfgpetclinic.junitextensions.TimingExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 // Maven by default detects Integration tests when they end with the IT letters
+@ExtendWith(TimingExtension.class)
 class PetTypeSDJpaServiceIT {
 
     @BeforeEach
